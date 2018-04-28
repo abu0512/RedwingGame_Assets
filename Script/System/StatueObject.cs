fce7e24f2a4a016cbe2b1867a22de791a8d0826c
Ahn <quddnr0512@gmail.com>
2018-04-29 07:04:33 +0900

    no message

--- Script/System/StatueObject.cs
+++ Script/System/StatueObject.cs
@@ -7,40 +7,51 @@ public class StatueObject : MonoBehaviour
     private Animator _anim;
     [SerializeField]
     private GameObject _destroy;
+    [SerializeField]
+    private GameObject _laser;
+    [SerializeField]
+    private GameObject _center;
     private float _destTime;
     private bool _dest;
+    private bool _laserCrash;
 
     public GameObject DestroyEffect { get { return _destroy; } }
 
 	void Start ()
     {
         _anim = GetComponent<Animator>();
+        _laser.SetActive(false);
     }
 	
 	void Update ()
     {
         DestroyUpdate();
-
     }
 
     private void DestroyUpdate()
     {
-        if (!_dest)
+        if (_dest)
             return;
 
-        _destTime += Time.deltaTime;
-
-        if (_destTime < 1.5f)
-            return;
-
-        _destroy.SetActive(false);
+        _laser.transform.LookAt(_center.transform.position);
+        Vector3 scale = _laser.transform.localScale;
+        scale.z += 0.1f;
+        _laser.transform.localScale = scale;
+        print("AAAAAA");
     }
 
     public void SetDestroyEffect()
     {
         _destroy.SetActive(true);
+        StartCoroutine(Co_EffectOff());
         _dest = true;
-        _destTime = 0.0f;
         _anim.SetBool("On", true);
+    }
+
+    IEnumerator Co_EffectOff()
+    {
+        yield return new WaitForSeconds(1.5f);
+        _destroy.SetActive(false);
+        _laser.SetActive(true);
     }
 }
