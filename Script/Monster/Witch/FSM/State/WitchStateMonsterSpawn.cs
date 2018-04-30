d5e951ee0af38ea5626430c7feb41e7dac14af61
Ahn <quddnr0512@gmail.com>
2018-05-01 00:56:28 +0900

    no message

---
+++ Script/Monster/Witch/FSM/State/WitchStateMonsterSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchStateMonsterSpawn : WitchFSMStateBase
+{
+
+    public override void BeginState()
+    {
+        Witch.SkillSys.OnTeleport(transform, 2);
+    }
+
+    void Update ()
+    {
+		
+	}
+
+    public override void EndState()
+    {
+    }
+}
